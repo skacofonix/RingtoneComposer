a217361c464ec21d96cce69de8bebd4c535f2adb
Yannick <yannick.valy@gmail.com>
2014-04-19 09:08:25 +0200

    Init RttlConverterTest

---
+++ RingtoneComposer.Test/Converter/RttlConverterTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RingtoneComposer.Core.Converter;
+
+namespace RingtoneComposer.Test.Converter
+{
+    [TestClass]
+    public class RttlConverterTest
+    {
+        private RttlConverter converter = new RttlConverter();
+
+        [TestMethod]
+        public void ToString_Success()
+        {
+            Assert.IsTrue(false, "Not yet implemented");
+        }
+
+        [TestMethod]
+        public void Parse_Success()
+        {
+            var partition = "";
+
+            var tune = converter.Parse(partition);
+
+            Assert.IsNotNull(tune);
+
+            Assert.IsTrue(tune.TuneElementList != null && tune.TuneElementList.Count > 0);
+        }
+    }
+}
