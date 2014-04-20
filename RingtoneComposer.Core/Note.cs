1d9a0deacfb7671c72bb155d947353c563de77e6
Yannick <yannick.valy@gmail.com>
2014-04-20 12:44:53 +0200

    Implement NokiaKeypressConverter

--- RingtoneComposer.Core/Note.cs
+++ RingtoneComposer.Core/Note.cs
@@ -5,6 +5,34 @@
         public Pitches Pitch { get; set; }
         public Scales Scale { get; set; }
 
+        public bool IsSharpable
+        {
+            get
+            {
+                if (Pitch == Pitches.B || Pitch == Pitches.E)
+                    return false;
+                return true;
+            }
+        }
+
+        public bool IsSharp 
+        {
+            get
+            {
+                switch(Pitch)
+                {
+                    case Pitches.Asharp:
+                    case Pitches.Csharp:
+                    case Pitches.Dsharp:
+                    case Pitches.Fsharp:
+                    case Pitches.Gsharp:
+                        return true;
+                    default :
+                        return false;
+                }
+            }
+        }
+
         public Note()
         { }
 
---
+++ RingtoneComposer.Test/Converter/NokiaKeypressConverterTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RingtoneComposer.Core.Converter;
+
+namespace RingtoneComposer.Test.Converter
+{
+    [TestClass]
+    public class NokiaKeypressConverterTest
+    {
+        private NokiaKeypressConverter converter = new NokiaKeypressConverter();
+
+        [TestMethod]
+        public void Parse_Success()
+        {
+            var partition = "(6)888#* (5)# 69999# 58888# 4# 4 (2)# (2)999 29# (6)8888# (5)# 69999# 488 4# 2 299# 288 4 5# 7 2* 49 (5)#** (4) 599 088888 88888";
+
+            var tune = converter.Parse(partition);
+
+            Assert.IsNotNull(tune);
+
+            Assert.IsTrue(tune.TuneElementList != null && tune.TuneElementList.Count > 0);
+        }
+    }
+}
